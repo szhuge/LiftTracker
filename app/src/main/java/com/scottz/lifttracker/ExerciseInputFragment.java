485cfacc0b00e3dde171883affa38074e1c76cf1
scottz <scottz@dropbox.com>
2017-07-25 13:14:44 -0700

    Convert to Fragments

---
+++ app/src/main/java/com/scottz/lifttracker/ExerciseInputFragment.java
@@ -0,0 +1,25 @@
+package com.scottz.lifttracker;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class ExerciseInputFragment extends Fragment {
+
+    private EditText mWeight;
+    private EditText mReps;
+
+    public ExerciseInputFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_exercise_input, container, false);
+    }
+}
